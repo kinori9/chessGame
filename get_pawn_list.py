082a9df8105708bacf71cc86d1cde2a065c7a172
kinori9 <kinori927@gmail.com>
2021-07-20 23:12:27 +0900

    pawn fix

--- get_pawn_list.py
+++ get_pawn_list.py
@@ -25,7 +25,6 @@ def get_pawn_list(x, y, color):
                     for w_name, w_piece in white_piece.items():
                         if (w_piece.x == x and w_piece.y == y - i) or (b_piece.x == x and b_piece.y == y - 1):
                             not_found = False
-
                 if not_found == True:
                     can_move_list.append(str(x) + ',' + str(y - i))
                 else:
@@ -36,11 +35,11 @@ def get_pawn_list(x, y, color):
             if piece.x == x + 1 and piece.y == y - 1:
                 can_move_list.append(str(x + 1) + ',' + str(y - 1))
     else: # if color == 'black':
-        if y == 1:
+        if y == 1: #예외 처리맨처음 두칸 가능한 경우:
             for i in range(1, 3):
                 for w_name, w_piece in white_piece.items():
                     for b_name, b_piece in black_piece.items():
-                        if (w_piece.x == x and w_piece.y == y + i) or (b_piece.x == x and b_piece.y == y + 1):
+                        if (b_piece.x == x and b_piece.y == y + i) or (w_piece.x == x and w_piece.y == y + 1):
                             not_found = False
                 if not_found == True:
                     can_move_list.append(str(x) + ',' + str(y + i))
